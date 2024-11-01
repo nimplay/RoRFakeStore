26c49b0f1779896bfe357d4a13172177e9e76bc2
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2024-11-01 11:12:06 -0400

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#home'
+  # Ruta para el controlador Content
+  get '/content', to: 'content#content', as: 'content'
+
+  # Ruta para el controlador Store
+  get '/store', to: 'store#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
