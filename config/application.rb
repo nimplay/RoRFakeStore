81ae0bb32b2078d3cf529b3c5f7cfed1dfa20e86
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2024-11-06 22:06:30 -0400

    resurrection

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,15 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module FakeStore
+module Storefake
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # Configuration for the application, engines, and railties goes here.
     #
--- test/controllers/content_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ContentControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
